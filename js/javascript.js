6033a96ed16c6ee71ca6479b98441b820644be4c
Thitus99 <242421157+Thitusthemaker@users.noreply.github.com>
2026-04-13 14:45:41 +0200

    v.15?

--- js/javascript.js
+++ js/javascript.js
@@ -1,7 +1,21 @@
 var menubutton = document.querySelector("#menu-button").addEventListener("click",toggleMenu)
+var closebutton = document.querySelector("#close-menu").addEventListener("click",toggleMenu)
 
+var starslider = document.getElementById("starsSlider")
+const stars = document.getElementById("stars")
+
+var minPriceSlider = document.getElementById("minPriceSlider")
+const minPrice = document.getElementById("minPrice")
 
 function toggleMenu(){
     let menu = document.querySelector("#menu")
     menu.classList.toggle("show")
+}
+
+starslider.oninput = function(){
+    stars.textContent = 'Minimum stars: ' + this.value
+}
+
+minPriceSlider.oninput = function(){
+    minPrice.textContent = 'Minium price: ' + this.value + 'kr'
 }
